5c3e63d86dc5e4b311a5c21649c4a3c3e13d3ae4
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-05-29 11:00:25 -0300

    feat: implementing entities

---
+++ BloodDonation/BloodDonation.Core/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace BloodDonation.Core.Entities;
+public abstract class BaseEntity()
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public DateTime CreatedAt { get; private set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; private set; }
+
+    public void LastUpdatedAt(DateTime updatedAt)
+    {
+        UpdatedAt = updatedAt;
+    }
+}
---
+++ BloodDonation/BloodDonation.Core/Entities/BloodStock.cs
@@ -0,0 +1,16 @@
+﻿using BloodDonation.Core.Enums;
+
+namespace BloodDonation.Core.Entities;
+public class BloodStock : BaseEntity
+{
+    public BloodStock(BloodTypesEnum bloodType, RhFatorEnum rhFactor, int mLQuantity)
+    {
+        BloodType = bloodType;
+        RhFactor = rhFactor;
+        MLQuantity = mLQuantity;
+    }
+
+    public BloodTypesEnum BloodType { get; private set; }
+    public RhFatorEnum RhFactor { get; private set; }
+    public int MLQuantity { get; private set; }
+}
