dfd9b727c05e542572bf66a87ae25585f0f20975
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-06-04 12:28:46 -0300

    feat: implementing entities configurations with fluent api

---
+++ BloodDonation/BloodDonation.Infra/Persistence/Configurations/DonationConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using BloodDonation.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodDonation.Infra.Persistence.Configurations;
+public class DonationConfiguration : IEntityTypeConfiguration<Donation>
+{
+    public void Configure(EntityTypeBuilder<Donation> builder)
+    {
+        builder.HasKey(d => d.Id);
+
+        builder.Property(d => d.DonationDate).IsRequired();
+        builder.Property(d => d.MLQuantity).IsRequired();
+    }
+}
---
+++ BloodDonation/BloodDonation.Infra/Persistence/Configurations/DonorConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using BloodDonation.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodDonation.Infra.Persistence.Configurations;
+public class DonorConfiguration : IEntityTypeConfiguration<Donor>
+{
+    public void Configure(EntityTypeBuilder<Donor> builder)
+    {
+        builder.HasKey(d => d.Id);
+
+        builder.Property(d => d.Name).HasMaxLength(200).IsRequired();
+        builder.Property(d => d.Email).HasMaxLength(100).IsRequired();
+        builder.Property(d => d.BirthdayDate).IsRequired();
+        builder.Property(d => d.Gender).HasConversion<string>().IsRequired();
+        builder.Property(d => d.Weight).IsRequired();
+        builder.Property(d => d.BloodType).HasConversion<string>().IsRequired();
+        builder.Property(d => d.RhFact).HasConversion<string>().IsRequired();
+    }
+}
