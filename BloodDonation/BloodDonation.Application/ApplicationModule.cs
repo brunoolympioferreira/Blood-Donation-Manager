e2e44515b718ec351732176a985ae24f4f4572e7
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-06-12 10:29:35 -0300

    feat: generating application module scope

---
+++ BloodDonation/BloodDonation.Application/ApplicationModule.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BloodDonation.Application;
+public static class ApplicationModule
+{
+    public static void AddApplicationModule(this IServiceCollection services)
+    {
+
+    }
+}
