441bd3ff19d0fdaabd62400cdd079144a31b71f6
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-07-03 16:23:42 -0300

    feat: implementing feature get donation by donor id

--- BloodDonation/BloodDonation.Application/Services/Donation/IDonationService.cs
+++ BloodDonation/BloodDonation.Application/Services/Donation/IDonationService.cs
@@ -1,7 +1,9 @@
 ﻿using BloodDonation.Application.Models.InputModels.Donation;
+using BloodDonation.Application.Models.ViewModels.Donation;
 
 namespace BloodDonation.Application.Services.Donation;
 public interface IDonationService
 {
     Task<Guid> Register(DonationInputModel model);
+    Task<List<DonationViewModel>> GetByDonor(Guid donorId);
 }
--- BloodDonation/BloodDonation.Infra/Persistence/Repositories/DonationRepository.cs
+++ BloodDonation/BloodDonation.Infra/Persistence/Repositories/DonationRepository.cs
@@ -1,5 +1,6 @@
 ﻿using BloodDonation.Core.Entities;
 using BloodDonation.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace BloodDonation.Infra.Persistence.Repositories;
 public class DonationRepository(BloodDonationDbContext context) : IDonationRepository
@@ -9,5 +10,12 @@ public class DonationRepository(BloodDonationDbContext context) : IDonationRepos
     public async Task AddAsync(Donation donation)
     {
         await _context.AddAsync(donation);
+    }
+
+    public async Task<List<Donation>> GetByDonorIdAsync(Guid donorId)
+    {
+        List<Donation> donation = await _context.Donations.Where(d => d.DonorId == donorId).ToListAsync();
+
+        return donation;
     }
 }
