8f5b0f9c4aee3059fe10613938f470c18baa2a9d
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-10-04 10:40:56 -0300

    implementing home works

--- BloodDonationUI/blood-donation/src/app/app.module.ts
+++ BloodDonationUI/blood-donation/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { NavigationModule } from './components/navigation/navigation/navigation.module';
+import { NavigationModule } from './components/navigation/navigation.module';
 
 @NgModule({
   declarations: [
