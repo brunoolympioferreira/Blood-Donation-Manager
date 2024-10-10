04d7d7153d2b4c97e3b1cc1b73ca89269d5e604d
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-10-10 12:15:23 -0300

    feat: iniciando implementacao form

--- BloodDonationUI/src/app/features/login/components/login-form/login-form.component.ts
+++ BloodDonationUI/src/app/features/login/components/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  getEmailErrorMessage() {
+  }
+
+  onSubmit() {
+
+  }
 }
